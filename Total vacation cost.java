98c5b04b3a2c75e5058b47e135c60f5016b6b2ae
safariderick353-bit <safariderick353@gmail.com>
2025-11-17 18:48:38 +0300

    Add TotalVacationCost class to calculate vacation cost

---
+++ Total vacation cost.java
@@ -0,0 +1,12 @@
+public class TotalVacationCost {
+    public static void main(String[] args) {
+
+        double accommodation = 500.00;
+        double meals = 200.00;
+        double activities = 300.00;
+
+        double totalCost = accommodation + meals + activities;
+
+        System.out.println("Total vacation cost: $" + totalCost);
+    }
+}
